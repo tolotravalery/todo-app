c10f5b43a663cb8ab649543eb231086ee93de118
tolotravalery <valeryharisoanitolotra@gmail.com>
2019-04-19 15:30:03 +0300

    /todo-app/dist/todo-ap/index.html

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'todo-app/dist')));
  
 //Catch all other routes and return to the index file
 app.get('*', (req, res) =>{
-   res.sendFile(path.join(__dirname, 'todo-app/dist/index.html'));
+   res.sendFile(path.join(__dirname, 'todo-app/dist/todo-app/index.html'));
 });
 
 //Get environment port or use 3000
